37862c70331a2dff2b74f94abff13151e5acd555
Diogo Augusto <diogo.augustoo@gmail.com>
2018-01-08 16:35:56 -0200

    Added Post and PostList Component

--- src/app/blog/Blog.jsx
+++ src/app/blog/Blog.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
+import PostList from './post/PostList';
 
 const Blog = () => (
   <div className='container'>
-    <div className='row justify-content-center'>
-
+    <h1>Blog</h1>
+    <div className={classNames('row', 'justify-content-center')}>
+      <div className='col-md-8'>
+        <PostList />
+      </div>
     </div>
   </div>
 );
---
+++ src/app/blog/post/PostList.jsx
@@ -0,0 +1,12 @@
+import React from  'react';
+import Post from './Post';
+
+const PostList = () => (
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+);
+
+export default PostList;
